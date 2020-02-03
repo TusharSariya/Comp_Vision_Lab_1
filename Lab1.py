675f92ee7705bc29b53eae5e00edc35ffe36f082
Tushar Sariya <tushar.97@hotmail.com>
2020-02-03 15:35:07 -0330

    added video

--- Lab1.py
+++ Lab1.py
@@ -8,10 +8,6 @@ Created on Mon Feb  3 01:32:57 2020
 import cv2
 import numpy as np
 import argparse
-class point:
-    def __init__(self,c0,c1):
-        self.c0 = c0
-        self.c1 = c1
 
 class line:
     def __init__(self, x0, y0, x1, y1):
@@ -20,11 +16,12 @@ class line:
         self.x1 = x1
         self.y1 = y1
 
+#cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
 def viewImage(image):
-    cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+    #cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
     cv2.imshow('Display', image)
-    cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    #cv2.waitKey(0)
+    #cv2.destroyAllWindows()
 
 def line_intersection(line1, line2):
     def det(a, b, c, d):
@@ -32,6 +29,9 @@ def line_intersection(line1, line2):
 
     bottom = (line1.x0-line1.x1)*(line2.y0-line2.y1) - (line1.y0-line1.y1)*(line2.x0 - line2.x1)
 
+    if bottom == 0:
+        return ( -50,-50)
+    
     a = det(line1.x0, line1.y0, line1.x1, line1.y1)
     b = det(line1.x0, 1, line1.x1, 1)
     c = det(line2.x0, line2.y0, line2.x1, line2.y1)
@@ -51,37 +51,45 @@ def line_intersection(line1, line2):
 #args = vars(ap.parse_args())
  
 #img = cv2.imread(args["image"])
-img = cv2.imread("test1.JPG")
-size = img.shape
-viewImage(img)
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
-edges = cv2.Canny(gray,100,150)# The parameters are the thresholds for Canny
+#img = cv2.imread("test1.JPG")
+cv2.namedWindow('Display', cv2.WINDOW_NORMAL)
+cap = cv2.VideoCapture("Movie1.MOV")
+while(cap.isOpened()):
+    hasframe, img = cap.read()
+    size = img.shape
+    viewImage(img)
+    gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+    edges = cv2.Canny(gray,100,150)# The parameters are the thresholds for Canny
 
-lines = cv2.HoughLines(edges,0.5,0.01,300) # The parameters are accuracies and threshold
-num = len(lines)
-line_points = []
-for n in range(num):
-    rho, theta = lines[n][0]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a*rho
-    y0 = b*rho
-    x1 = int(x0 + size[1]*(-b))
-    y1 = int(y0 + size[0]*(a))
-    x2 = int(x0 - size[1]*(-b))
-    y2 = int(y0 - size[0]*(a))
+    lines = cv2.HoughLines(edges,0.5,0.01,300) # The parameters are accuracies and threshold
+    num = len(lines)
+    line_points = []
+    for n in range(num):
+        rho, theta = lines[n][0]
+        a = np.cos(theta)
+        b = np.sin(theta)
+        x0 = a*rho
+        y0 = b*rho
+        x1 = int(x0 + size[1]*(-b))
+        y1 = int(y0 + size[0]*(a))
+        x2 = int(x0 - size[1]*(-b))
+        y2 = int(y0 - size[0]*(a))
 
-    cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
+        cv2.line(img,(x1,y1),(x2,y2),(0,0,255),2)
 
-    line_points.append(line(x0,y0,x1,y1))
+        line_points.append(line(x0,y0,x1,y1))
 
-viewImage(img)
-cv2.imwrite('houghlines1.jpg',img)
+    viewImage(img)
+    cv2.imwrite('houghlines1.jpg',img)
 
-for n in range(num-1):
-    for j in range(n+1,num):
-        intersection = line_intersection(line_points[n],line_points[j])
-        cv2.circle(img, intersection, 25, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
+    for n in range(num-1):
+        for j in range(n+1,num):
+            intersection = line_intersection(line_points[n],line_points[j])
+            print(intersection[0]," ",intersection[1])
+            if(intersection[0]<3000 and intersection[0]<3000):
+                cv2.circle(img, intersection, 25, (0, 255, 255), thickness=-1, lineType=cv2.FILLED)
 
-viewImage(img)
-cv2.imwrite('intersection.jpg',img)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    viewImage(img)
+    cv2.imwrite('intersection.jpg',img)
